db7a1eb309e829013935f430652442b7fa2e7dc3
unknown <jupanecasek@gmail.com>
2024-11-13 21:36:14 -0300

    Arreglo en app.py

--- promtior-bot/app.py
+++ promtior-bot/app.py
@@ -15,4 +15,4 @@ def messages():
     return jsonify({"response": output["final_response"]})
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=8000)
